1394152117b60d3ae4c05c0573c9702dac3539f3
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-12 04:34:08 -0700

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
 from .utils import get_all_properties  # Import your caching function
+from django.views.decorators.cache import cache_page
+from .models import Property
 
+@cache_page(60 * 15)
 def property_list(request):
     """
-    Return all properties from cache or DB.
+    Returns all property listings as JSON.
+    The entire response is cached in Redis for 15 minutes.
     """
-    data = get_all_properties()  # Use the cached or fresh queryset
-    return JsonResponse(data, safe=False) 
+    properties = Property.objects.all().values()  # Query all properties from the database
+    data = list(properties)  # Convert the queryset to a list of dictionaries
+    return JsonResponse(data, safe=False)  # Return as JSON response
